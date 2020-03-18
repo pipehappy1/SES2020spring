bef140a84e5f09fbb76c861add4bac5bea8540fb
Rapunzel-hair <61175678+Rapunzel-hair@users.noreply.github.com>
2020-03-18 13:11:03 +0800

    Update Unit_3.py

--- unit4/tests/Unit_3.py
+++ unit4/tests/Unit_3.py
@@ -23,46 +23,50 @@ class circle:
         self.radius = radius
         self.x = x
         self.y = y
-        
+    
+#打印圆的半径和圆心坐标    
     def print_circle(self):
         print('radius={}, coordinate=({},{})'.format(self.radius, self.x, self.y))
     
-    #计算两圆心之间距离
-    def distance(self, c2):       
-        dis = ((self.x-c2.x)**2+(self.y-c2.y)**2)**0.5
+#计算两圆心之间的距离
+    def distance(c1, c2):       
+        dis = ((c1.x-c2.x)**2+(c1.y-c2.y)**2)**0.5
         return dis
     
-    #判断新圆与现存圆是否相交，相交为0，全不相交为1 
+#判断新圆与现存圆是否相交，相交为0，全不相交为1 
     def ifcross(self, c_list):
-        for i in range (len(c_list)):
-            c2 = c_list[i]
+        for c in c_list:
             r1 = self.radius
-            r2 = c2.radius
+            r2 = c.radius
             rr = r1+r2
-            dis = self.distance(c2)
+            dis = self.distance(c)
             if dis < rr:
                 return 0
         return 1
         
-    #判断圆是否越界，越界为0，不越界为1 
+#判断圆是否越界，越界为0，不越界为1 
     def ifexcess(self):
-        r = self.radius
         x = self.x
-        y = self.y         
-        if x + r > 1 or x - r < -1 or y + r > 1 or y - r < -1:
+        y = self.y
+        r = self.radius
+        left = abs(x - r)
+        right = abs(x + r)
+        upper = abs(y + r)
+        lower = abs(y - r)
+        if max(left, right, upper, lower) > 1:
             return 0
-        else:
-            return 1
+        return 1
 
-#找出可行的最大半径
+#找出最大可行半径
 def MaxR(c1, c_list):
     x = c1.x
     y = c1.y
     R_list = [1-x,1+x,1-y,1+y]
-    for i in range (len(c_list)):
-        c2 = c_list[i]
-        dis = c1.distance(c2)
-        R_list.append(dis-c2.radius)
+# 计算到其他各圆的距离
+    for c in c_list:
+            r = c.distance(c1) - c.radius
+            R_list.append(r)
+# 从中取最小值，即最大可行半径
     return min(R_list)
 
 #需要优化的目标函数        
@@ -88,11 +92,13 @@ def opt_center(c, c_list):
 #找m个圆，使得每个圆在邻域内半径最大
 def FindMaxCircuit(m):
     c_list = []
+#逐步添加新的圆
     for i in range (m):
         r = 0
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
         c = circle(r, x, y)
+#没有圆相交
         while not c.ifcross(c_list):           
             x = random.uniform(-1, 1)
             y = random.uniform(-1, 1)
@@ -107,12 +113,12 @@ def plot(c_list):
     plt.xlim([-1,1])
     plt.ylim([-1,1])  
     theta = np.linspace(0,2*np.pi,50)
-    for c in c_list:
-        plt.plot(c.x+c.radius*np.cos(theta),c.y+c.radius*np.sin(theta),'b')       
+    for c in c_list: 
+        plt.plot(c.x+c.radius*np.cos(theta),c.y+c.radius*np.sin(theta),'b') 
     plt.show()
     
 if __name__ == "__main__":
-    m = 10
+    m = 30
     c_list = FindMaxCircuit(m)   
     RR = 0
     for c in c_list:
