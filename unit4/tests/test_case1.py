59ad5fa4ddc58b6c66deacaeba5969fb5374967b
Rapunzel-hair <61175678+Rapunzel-hair@users.noreply.github.com>
2020-03-18 13:02:45 +0800

    Update test_case1.py

--- unit4/tests/test_case1.py
+++ unit4/tests/test_case1.py
@@ -1,18 +1,22 @@
 import unittest
 import numpy as np
-from Unit_3 import circle
-from Unit_3 import *
+from FindMCircle import circle
+from FindMCircle import *
 
 class TestCircle(unittest.TestCase):
     def setUp(self):
         self.c1 = circle(1,0,0)
-        self.c2 = circle(1,1,0)
+        self.c2 = circle(0.5,0.5,0.5)
         self.c3 = circle(1,1,1)
-        self.c4 = circle(0.5,-0.5,-0.5)
+        self.c4 = circle(0,-0.5,-0.5)
         self.c_list = []
         self.c_list.append(self.c2)
         self.c_list.append(self.c3)
-
+    
+    def test_dis(self):
+        dis_c = self.c1.distance(self.c2)
+        dis = np.linalg.norm([self.c1.x-self.c2.x,self.c1.y-self.c2.y])
+        self.assertEqual(dis_c,dis)
     
     def test_cross(self):
         self.assertEqual(self.c1.ifcross(self.c_list),0)
@@ -21,18 +25,13 @@ class TestCircle(unittest.TestCase):
         self.assertEqual(self.c1.ifexcess(),1)
         self.assertFalse(self.c3.ifexcess(),1)
         
-    def test_distance(self):
-        dis_c = self.c1.distance(self.c2)
-        dis = np.linalg.norm([self.c1.x-self.c2.x,self.c1.y-self.c2.y])
-        self.assertEqual(dis_c,dis)
-        
     def test_MaxR(self):
         MR = MaxR(self.c4,self.c_list)
         print(MR)
         self.assertLessEqual(MR,2)
         
     def test_FindMaxCircle(self):
-        m = 20
+        m = 10
         c_list = FindMaxCircuit(m)
         RR = 0
         for c in c_list:
